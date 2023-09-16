8d9617bc08d9a6f5081dcb16024ccf02756de64d
ivaldas <ivaldas@gmail.com>
2023-09-16 18:30:45 +0300

    updated

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import {
 	signInWithGooglePopup,
-	createUserDocumentFromAuth,
+	// createUserDocumentFromAuth,
 	signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
 import './sign-in-form.styles.scss';
@@ -22,15 +22,13 @@ const SignInForm = () => {
 	};
 
 	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup();
-		await createUserDocumentFromAuth(user);
+		await signInWithGooglePopup();
 	};
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		try {
-			const response = await signInAuthUserWithEmailAndPassword(email, password);
-			console.log(response);
+			await signInAuthUserWithEmailAndPassword(email, password);
 			resetFormFields();
 		} catch (error) {
 			switch (error.code) {
