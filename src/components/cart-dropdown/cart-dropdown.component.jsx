b2e1afb2856ef3a7d21ab270472dfb198df32990
ivaldas <ivaldas@gmail.com>
2023-10-02 14:02:24 +0300

    added nested routes and links

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -14,7 +14,7 @@ const CartDropdown = () => {
 	return (
 		<div className="cart-dropdown-container">
 			<div className="cart-items">{cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)}</div>
-			<Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
+			<Button onClick={goToCheckoutHandler}>Checkout</Button>
 		</div>
 	);
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
 	signOut,
 	onAuthStateChanged
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyChP5Abn8KWFslyA65pOguYEfxA0TjmWN8',
@@ -30,6 +30,30 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+	const collectionRef = collection(db, collectionKey);
+	const batch = writeBatch(db);
+	objectsToAdd.forEach((object) => {
+		const docRef = doc(collectionRef, object.title.toLowerCase());
+		batch.set(docRef, object);
+	});
+	await batch.commit();
+	console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+	const collectionRef = collection(db, 'categories');
+	const q = query(collectionRef);
+	const querySnapshot = await getDocs(q);
+	const categoryMap = querySnapshot.docs.reduce((accum, docSnapshot) => {
+		const { title, items } = docSnapshot.data();
+		accum[title.toLowerCase()] = items;
+		return accum;
+	}, {});
+	return categoryMap;
+};
+
 export const createUserDocumentFromAuth = async (userAuth, aditionalInfo = {}) => {
 	if (!userAuth) return;
 	const userDocRef = doc(db, 'users', userAuth.uid);
